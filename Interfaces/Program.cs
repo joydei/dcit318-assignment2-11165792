52ad6d61aab98a27d0ecd9da9934b80e4479f4d1
Joy <deiselormjoy@gmail.com>
2025-08-03 12:13:42 +0000

    Implement main program logic to demonstrate usage of IMovable interface with Car and Bicycle classes

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Demonstrating Interfaces");
+
+        IMovable myCar = new Car();
+        IMovable myBicycle = new Bicycle();
+        myCar.Move();
+        myBicycle.Move();
+        Console.WriteLine("Both Car and Bicycle implement the IMovable interface.");
+        Console.WriteLine("\nUsing a collection of IMovable objects");
+        IMovable[] movables = new IMovable[2];
+        movables[0] = new Car();
+        movables[1] = new Bicycle();
+
+        foreach (IMovable movable in movables)
+        {
+            movable.Move();
+        }
+    }
+}
