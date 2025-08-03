a1829e92020f2f076331bbf8e14d8cd64398ab91
Joy <deiselormjoy@gmail.com>
2025-08-03 11:59:04 +0000

    Add Circle and Rectangle classes with area calculation; update Program to demonstrate usage

---
+++ AbstractClassesMethods/Circle.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(double radius)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
--- AbstractClassesMethods/Program.cs
+++ AbstractClassesMethods/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Demonstrating Abstract Classes and Methods");
+
+        Circle myCircle = new Circle(5.0);
+        Console.WriteLine($"Area of the circle with radius 5: {myCircle.GetArea():F2}");
+
+        Rectangle myRectangle = new Rectangle(4.0, 6.0);
+        Console.WriteLine($"Area of the rectangle with sides 4 and 6: {myRectangle.GetArea():F2}");
+    }
+}
